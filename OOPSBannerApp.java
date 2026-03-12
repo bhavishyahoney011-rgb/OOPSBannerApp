35e576cc75802b10de7338f4528c95081ad54efe
Bhavishya <your-email@example.com>
2026-03-12 20:00:44 +0530

    Added OOPSBannerApp files

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -9,12 +9,30 @@ public class UC8BannerApp {
 
         // Step 2: Store patterns
 
-   public class KmToMiles {
+   import java.util.Scanner;
+
+public class BasicCalculator {
     public static void main(String[] args) {
 
-        double km = 10.8;
-        double miles = km * 1.6;
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("The distance " + km + " km in miles is " + miles);
+        // Taking user input
+        System.out.print("Enter first number: ");
+        double number1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double number2 = sc.nextDouble();
+
+        // Arithmetic operations
+        double addition = number1 + number2;
+        double subtraction = number1 - number2;
+        double multiplication = number1 * number2;
+        double division = number1 / number2;
+
+        // Printing results
+        System.out.println("Addition = " + addition);
+        System.out.println("Subtraction = " + subtraction);
+        System.out.println("Multiplication = " + multiplication);
+        System.out.println("Division = " + division);
     }
 }
