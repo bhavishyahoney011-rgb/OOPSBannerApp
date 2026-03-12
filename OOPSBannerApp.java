fc3ab4e05abb74bc6306f07e033f82a9dad5d603
Bhavishya <your-email@example.com>
2026-03-12 20:04:30 +0530

    Added OOPSBannerApp files

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -11,25 +11,19 @@ public class UC8BannerApp {
 
     import java.util.Scanner;
 
-public class TriangleArea {
+public class TemperatureConversion {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        // Taking input
-        System.out.print("Enter base in cm: ");
-        double base = sc.nextDouble();
+        // Taking Celsius input
+        System.out.print("Enter temperature in Celsius: ");
+        double celsius = sc.nextDouble();
 
-        System.out.print("Enter height in cm: ");
-        double height = sc.nextDouble();
+        // Convert to Fahrenheit
+        double fahrenheitResult = (celsius * 9/5) + 32;
 
-        // Area in square centimeters
-        double areaCm = 0.5 * base * height;
-
-        // Convert to square inches
-        double areaIn = areaCm / 6.4516;
-
-        // Output
-        System.out.println("The Area of the triangle in sq in is " + areaIn + " and sq cm is " + areaCm);
+        // Print result
+        System.out.println("The " + celsius + " celsius is " + fahrenheitResult + " fahrenheit");
     }
 }
