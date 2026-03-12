70afb68f9856e62bbf2137c6a9ba01ae642b1b91
Bhavishya <your-email@example.com>
2026-03-12 19:25:37 +0530

    Added OOPSBannerApp files

---
+++ OOPSBannerApp.java
@@ -0,0 +1,22 @@
+ import java.util.HashMap;
+
+public class UC8BannerApp {
+
+    public static void main(String[] args) {
+
+        // Step 1: Create HashMap
+        HashMap<Character, String[]> patternMap = new HashMap<>();
+
+        // Step 2: Store patterns
+
+  public class HarryAge {
+    public static void main(String[] args) {
+
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthYear;
+
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
