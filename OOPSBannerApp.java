52247ab70d4053bef15e1ece365270d616400321
Bhavishya <your-email@example.com>
2026-03-12 20:12:34 +0530

    Added OOPSBannerApp files

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -11,20 +11,26 @@ public class UC8BannerApp {
 
     import java.util.Scanner;
 
-public class DivisibleByFive {
+public class SmallestNumberCheck {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         // Taking input
-        System.out.print("Enter a number: ");
-        int number = sc.nextInt();
+        System.out.print("Enter first number: ");
+        int number1 = sc.nextInt();
 
-        // Check divisibility
-        if (number % 5 == 0) {
-            System.out.println("Is the number " + number + " divisible by 5? Yes");
+        System.out.print("Enter second number: ");
+        int number2 = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int number3 = sc.nextInt();
+
+        // Checking if first number is smallest
+        if (number1 < number2 && number1 < number3) {
+            System.out.println("Is the first number the smallest? Yes");
         } else {
-            System.out.println("Is the number " + number + " divisible by 5? No");
+            System.out.println("Is the first number the smallest? No");
         }
     }
 }
